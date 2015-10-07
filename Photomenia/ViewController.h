2d97d74c12cb962cf89f7fbb0a4f13161e9a6e71
Eugenio Penate <eugenio.penate@timeset.com>
2015-10-07 10:41:46 -0400

    Initial Commit

---
+++ Photomenia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Photomenia
+//
+//  Created by Eugenio Penate on 10/7/15.
+//  Copyright © 2015 TimeSet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Photomenia/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Photomenia
+//
+//  Created by Eugenio Penate on 10/7/15.
+//  Copyright © 2015 TimeSet. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
